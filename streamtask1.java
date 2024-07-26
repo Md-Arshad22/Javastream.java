c38b6497dc0b85b89b5c8f9c12594be1f91cb7ac
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-26 10:15:56 +0530

    Update streamtask1.java

--- streamtask1.java
+++ streamtask1.java
@@ -1,1 +1,23 @@
+package javaclasses;
 
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Ques2 {
+
+	public static void main(String[] args) {
+		
+List<Integer> numbers = List.of(10, 5, 20, 15, 25, 30);
+        
+        int secondSmallest = numbers.stream().sorted().distinct().skip(1).findFirst()
+                                    .orElseThrow(() -> new IllegalArgumentException("List is too small"));
+        int secondLargest = numbers.stream().sorted(Comparator.reverseOrder()).distinct().skip(1).findFirst()
+                                   .orElseThrow(() -> new IllegalArgumentException("List is too small"));
+
+        System.out.println("Second smallest element: " + secondSmallest);
+        System.out.println("Second largest element: " + secondLargest);
+		
+	}
+
+}
