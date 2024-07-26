4b75ef51d99adcc94c95d91319c4c6836f2f2ac5
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-26 10:21:58 +0530

    Update streamtask3.java

--- streamtask3.java
+++ streamtask3.java
@@ -2,7 +2,7 @@ package javaclasses;
 import java.util.Arrays;
 import java.util.List;
 
-public class Ques4 {
+public class streamtask3 {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		int sum = list.stream().reduce(0, Integer::sum);
