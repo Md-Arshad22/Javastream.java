988024372f457fc9d7bd61be83c3f28355036819
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-26 10:15:06 +0530

    Update streamtask.java

--- streamtask.java
+++ streamtask.java
@@ -1,1 +1,11 @@
-
+package javaclasses;
+import java.util.List;
+import java.util.stream.Collectors;
+	
+	public class Ques1 {
+	    public static void main(String[] args) {
+	        List<String> stringList = List.of("1", "2", "3");      
+	        List<Integer> integerList = stringList.stream().map(Integer::parseInt).collect(Collectors.toList());
+	        System.out.println("List of integers: " + integerList + stringList);
+	    }
+	  }
