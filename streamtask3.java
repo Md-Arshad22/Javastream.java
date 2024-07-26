ad32bfecd9e59c2e32dd5086a174f0d04896de40
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-26 10:25:57 +0530

    Update streamtask3.java

--- streamtask3.java
+++ streamtask3.java
@@ -2,7 +2,6 @@ package javaclasses;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class streamtask3 {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
