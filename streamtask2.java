29d239a490fae785301c6e218b58870f364d1602
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-26 10:16:20 +0530

    Update streamtask2.java

--- streamtask2.java
+++ streamtask2.java
@@ -1,1 +1,15 @@
+package javaclasses;
+import java.util.Arrays;
+import java.util.List;
 
+public class Ques3 {
+
+	public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        int EvenNumbers = numbers.stream().filter(num -> num % 2 == 0).mapToInt(Integer::intValue).sum();
+        int OddNumbers = numbers.stream().filter(num -> num % 2 != 0).mapToInt(Integer::intValue).sum();
+		        System.out.println("Sum of even numbers: " + EvenNumbers);
+		        System.out.println("Sum of odd numbers: " + OddNumbers);
+		    }
+
+	}
