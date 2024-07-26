c544f4edfe4bb4c15099adbad77f580d33cedcd2
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-26 10:36:24 +0530

    Update streamtask2.java

--- streamtask2.java
+++ streamtask2.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class streamtask2 {
-
 	public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         int EvenNumbers = numbers.stream().filter(num -> num % 2 == 0).mapToInt(Integer::intValue).sum();
