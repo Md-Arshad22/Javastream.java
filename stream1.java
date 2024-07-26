af5ab8d0689dccb77bf89dce095aa9cfac0e0b9d
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-26 16:26:45 +0530

    Update stream1.java

--- stream1.java
+++ stream1.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class Streamcls1 {
+public class stream1 {
 public static void main(String[] args) {
 	
 	            //REDUCE METHOD INTEGER IN STREAM
